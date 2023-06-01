52fd514133be1843bbb7bba8575ba23ccbbbb256
7rikaz_wsl1 <7rikaz.h785.stat2ltas41lcijad@gmail.com>
2023-06-01 23:39:36 +0900

    feat: Add sample test code

---
+++ calculator/operations.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def add(a, b):
+    return np.add(a, b)
+
+
+def subtract(a, b):
+    return np.subtract(a, b)
+
+
+def multiply(a, b):
+    return np.multiply(a, b)
+
+
+def divide(a, b):
+    return np.divide(a, b)
---
+++ tests/test_operations.py
@@ -0,0 +1,25 @@
+from calculator.operations import add, divide, multiply, subtract
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(0, 0) == 0
+    assert add(-2, 2) == 0
+
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 0) == 0
+    assert subtract(-2, 2) == -4
+
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(0, 10) == 0
+    assert multiply(-2, 4) == -8
+
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(10, 2) == 5
+    assert divide(7, 2) == 3.5
