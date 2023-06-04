d0da14948c63fb4d7a7db8d66e28a4e5935f96e0
7rikaz_wsl1 <7rikaz.h785.stat2ltas41lcijad@gmail.com>
2023-06-04 16:52:33 +0900

    feat: Support for feature by pre-commit and post-commit
    Closes #1

--- ci/run_git_tag_base_pyproject.py
+++ ci/run_git_tag_base_pyproject.py
@@ -9,6 +9,19 @@ from tomlkit.toml_file import TOMLFile
 
 
 def compare_versions(version1: str, version2: str) -> int:
+    """Compare specified versions
+
+    Args:
+        version1 (str): [major].[minor].[pathch]
+        version2 (str): [major].[minor].[pathch]
+
+    Returns:
+        Return comparison result
+        int:
+            v1 < v2: -1
+            v1 > v2: 1
+            v1 = v2: 0
+    """
     v1 = [int(num) for num in version1.split(".")]
     v2 = [int(num) for num in version2.split(".")]
     if v1 < v2:
@@ -20,9 +33,24 @@ def compare_versions(version1: str, version2: str) -> int:
 
 
 def remote_tag_checker(remote: str, tag: Optional[str]) -> bool:
+    """Checks if the string (git tag version) specified in the argument can be added to a remote tag
+
+    Args:
+        remote (str): remote repository
+        tag (Optional[str]): v[major].[minor].[pathch]
+
+    Raises:
+        SystemExit: invalid tag version
+        subprocess.CalledProcessError: Failed to fetch remote tags
+
+    Returns:
+        bool:
+            True: git add allowed
+            False: gitadd not allowed
+    """
     checker = False
     try:
-        # リモートブランチのタグを取得
+        # Get remote branch tags
         tags = subprocess.run(
             ["git", "ls-remote", "--tags", remote], capture_output=True, text=True
         ).stdout.splitlines()
@@ -51,9 +79,23 @@ def remote_tag_checker(remote: str, tag: Optional[str]) -> bool:
 
 
 def local_tag_checker(tag: Optional[str]) -> bool:
+    """Checks if the string (git tag version) specified in the argument can be added to a local tag
+
+    Args:
+        tag (Optional[str]): v[major].[minor].[pathch]
+
+    Raises:
+        SystemExit: invalid tag version
+        subprocess.CalledProcessError: no tags found
+
+    Returns:
+        bool:
+            True: git add allowed
+            False: gitadd not allowed
+    """
     checker = False
     try:
-        # タグを降順(n,n-1,n-2)にソートして取得
+        # Get tags sorted in descending order (n,n-1,n-2)
         tags = subprocess.run(
             ["git", "tag", "--sort", "-v:refname"], capture_output=True, text=True
         ).stdout.splitlines()
@@ -77,10 +119,20 @@ def local_tag_checker(tag: Optional[str]) -> bool:
 
 
 def read_poetry_project_version() -> Tuple[bool, Optional[str]]:
+    """Get the "version" value of the "poetry" key from "pyproject.toml"
+
+    Raises:
+        SystemExit:
+            pyproject.toml does not exist
+            version does not exist
+
+    Returns:
+        Tuple[bool, Optional[str]]: Acquisition decision result and [major]. [minor]. [pathch].
+    """
     read_success_flag = False
     curent_ver = ""
     try:
-        # pyproject.tomlファイルの読み込み
+        # Load the pyproject.toml file
         toml = TOMLFile("pyproject.toml")
         toml_data = toml.read()
         toml_get_data = toml_data.get("tool", {})
@@ -99,21 +151,31 @@ def read_poetry_project_version() -> Tuple[bool, Optional[str]]:
 
 
 def get_arg(tag: str) -> str:
+    """Version specified by command line arguments
+
+    Args:
+        tag (str): v[major].[minor].[pathch]
+
+    Raises:
+        SystemExit: Invalid tag format
+
+    Returns:
+        str: v[major].[minor].[pathch]
+    """
     if len(sys.argv) == 2:
         new_tag = sys.argv[1]
         pattern = r"^v[0-9]+\.[0-9]{1,3}\.[0-9]{1,3}$"
         if not re.match(pattern, new_tag):
-            # 引数の指定に誤りがある場合
+            # If an argument is incorrectly specified
             error_message = (
                 "Invalid tag format. Please enter in [vx.x.x]. Exit the program."
             )
             sys.exit(error_message)
     elif len(sys.argv) > 2:
-        error_message = "Please enter ./create_tag_data.py [vx.x.x] Exit the program."
+        error_message = "Invalid tag format. Please enter ./create_tag_data.py [vx.x.x] Exit the program."
         sys.exit(error_message)
     else:
         new_tag = tag
-    print(f"get_arg() return new_tag:{new_tag}")
     return new_tag
 
 
@@ -126,7 +188,7 @@ if __name__ == "__main__":  # pragma: no cover
     new_tag = get_arg(new_tag)
     remote_tag_checker_flag = remote_tag_checker(remote_to_check, new_tag)
     local_tag_checker_flag = local_tag_checker(new_tag)
-    # 新しいタグを作成する
+    # Create a new tag
     if remote_tag_checker_flag is True and local_tag_checker_flag is True:
         subprocess.run(["git", "tag", new_tag])
     subprocess.run(["git", "tag"])
--- ci/update_pyproject_version.py
+++ ci/update_pyproject_version.py
@@ -9,13 +9,22 @@ from tomlkit.toml_file import TOMLFile
 
 
 def update_poetry_project_version(new_tag: str, toml: TOMLFile) -> None:
+    """Update [poetry].[version] in pyproject.toml
+
+    Args:
+        new_tag (str): v[major].[minor].[pathch]
+        toml (TOMLFile): TOMLFile("pyproject.toml")
+
+    Raises:
+        KeyError: Failed to update pyproject.toml
+    """
     toml_data = toml.read()
     try:
-        # バージョンの更新
+        # Update version
         toml_get_data = toml_data.get("tool")
         if toml_get_data is not None and "poetry" in toml_get_data:
             toml_get_data["poetry"]["version"] = new_tag
-            # pyproject.tomlファイルの書き込み
+            # Writing the pyproject.toml file
             toml.write(toml_data)
         else:
             raise KeyError("Failed to find 'poetry' section in 'tool'")
@@ -26,11 +35,22 @@ def update_poetry_project_version(new_tag: str, toml: TOMLFile) -> None:
 
 
 def get_arg() -> Optional[str]:
+    """Version specified by command line arguments
+
+    Args:
+        tag (str): [major].[minor].[pathch]
+
+    Raises:
+        SystemExit: Invalid tag format([major].[minor].[pathch])
+
+    Returns:
+        str: [major].[minor].[pathch]
+    """
     if len(sys.argv) == 2:
         new_tag = sys.argv[1]
         pattern = r"^[0-9]+\.[0-9]{1,3}\.[0-9]{1,3}$"
         if not re.match(pattern, new_tag):
-            # 引数の指定に誤りがある場合
+            # If an argument is incorrectly specified
             error_message = (
                 "Invalid tag format. Please enter in [x.x.x]. Exit the program."
             )
@@ -44,29 +64,37 @@ def get_arg() -> Optional[str]:
 
 
 def create_ver(input_ver: Optional[str]) -> Tuple[bool, str, TOMLFile]:
-    # pyproject.tomlファイルの読み込み
+    """Create infomations of [poetry].[version] in pyproject.toml
+
+    Args:
+        input_ver (Optional[str]): [major].[minor].[pathch]
+
+    Returns:
+        Tuple[bool, str, TOMLFile]: Infomation of [poetry].[version] in pyproject.toml
+    """
+    # Load the pyproject.toml file
     toml = TOMLFile("pyproject.toml")
     toml_data = toml.read()
     toml_get_data = toml_data.get("tool")
-    # バージョンの更新
+    # Update version
     if toml_get_data is not None and "poetry" in toml_get_data:
         current_data = toml_get_data["poetry"].get("version")
     else:
         current_data = "0.0.0"
     major, minor, patch = map(int, current_data.split("."))
     create_tag_flag = False
-    # 引数指定のデータあり
+    # Argument specified data available
     if input_ver is not None:
         new_ver = input_ver
         create_tag_flag = True
-        # pyproject.tomlのversion以下の場合はエラー
+        # Error if less than version of pyproject.toml
         if input_ver <= current_data:
             create_tag_flag = False
             error_message = f"The specified tag '{input_ver}' must be greater than the latest tag 'v{current_data}'. Exit the program."
             sys.exit(error_message)
-    # 引数指定のデータなし
+    # No data specified in argument
     else:
-        # pyproject.tomlのversionをインクリメント
+        # Increment version in pyproject.toml
         if patch < 999:
             patch += 1
         elif minor < 999:
@@ -82,10 +110,10 @@ def create_ver(input_ver: Optional[str]) -> Tuple[bool, str, TOMLFile]:
 
 
 if __name__ == "__main__":  # pragma: no cover
-    # 正しいデータ形式かチェック
+    # Check for correct data format
     input_ver = get_arg()
-    # 引数なし：pyproject.tomlのversionをインクリメント
-    # 引数あり：pyproject.tomlのversion以上なら更新
+    # Without argument: Increment version of pyproject.toml
+    # With argument: update if version is greater than version of pyproject.toml
     create_tag_flag, new_ver, toml = create_ver(input_ver)
     if create_tag_flag:
         update_poetry_project_version(new_ver, toml)
