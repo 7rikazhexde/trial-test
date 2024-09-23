84034ce71c954ef3d5935c93a591a35cc2300ffa
7rikazhexde <7rikaz.h785.stat2ltas41lcijad@gmail.com>
2024-09-23 19:50:37 +0900

    :recycle:Fix CI script

--- ci/update_pyproject_version.py
+++ ci/update_pyproject_version.py
@@ -12,7 +12,7 @@ def update_poetry_project_version(new_tag: str, toml: TOMLFile) -> None:
     """Update [poetry].[version] in pyproject.toml
 
     Args:
-        new_tag (str): v[major].[minor].[pathch]
+        new_tag (str): [major].[minor].[patch]
         toml (TOMLFile): TOMLFile("pyproject.toml")
 
     Raises:
@@ -37,14 +37,11 @@ def update_poetry_project_version(new_tag: str, toml: TOMLFile) -> None:
 def get_arg() -> Optional[str]:
     """Version specified by command line arguments
 
-    Args:
-        tag (str): [major].[minor].[pathch]
-
     Raises:
-        SystemExit: Invalid tag format([major].[minor].[pathch])
+        SystemExit: Invalid tag format([major].[minor].[patch])
 
     Returns:
-        str: [major].[minor].[pathch]
+        Optional[str]: [major].[minor].[patch] or None if not specified
     """
     if len(sys.argv) == 2:
         new_tag = sys.argv[1]
@@ -56,7 +53,9 @@ def get_arg() -> Optional[str]:
             )
             sys.exit(error_message)
     elif len(sys.argv) > 2:
-        error_message = "Please enter ./create_tag_data.py [x.x.x] Exit the program."
+        error_message = (
+            "Please enter ./update_pyproject_version.py [x.x.x] Exit the program."
+        )
         sys.exit(error_message)
     else:
         new_tag = None
@@ -64,57 +63,43 @@ def get_arg() -> Optional[str]:
 
 
 def create_ver(input_ver: Optional[str]) -> Tuple[bool, str, TOMLFile]:
-    """Create infomations of [poetry].[version] in pyproject.toml
+    """Create information of [poetry].[version] in pyproject.toml
 
     Args:
-        input_ver (Optional[str]): [major].[minor].[pathch]
+        input_ver (Optional[str]): [major].[minor].[patch]
 
     Returns:
-        Tuple[bool, str, TOMLFile]: Infomation of [poetry].[version] in pyproject.toml
+        Tuple[bool, str, TOMLFile]: Information of [poetry].[version] in pyproject.toml
     """
     # Load the pyproject.toml file
     toml = TOMLFile("pyproject.toml")
     toml_data = toml.read()
     toml_get_data = toml_data.get("tool")
-    # Update version
+    # Get current version
     if toml_get_data is not None and "poetry" in toml_get_data:
         current_data = toml_get_data["poetry"].get("version")
     else:
         current_data = "0.0.0"
-    major, minor, patch = map(int, current_data.split("."))
-    create_tag_flag = False
-    # Argument specified data available
+
     if input_ver is not None:
-        new_ver = input_ver
-        create_tag_flag = True
-        # Error if less than version of pyproject.toml
+        # Check if input version is greater than current version
         if input_ver <= current_data:
-            create_tag_flag = False
-            error_message = f"The specified tag '{input_ver}' must be greater than the latest tag 'v{current_data}'. Exit the program."
+            error_message = f"The specified tag '{input_ver}' must be greater than the current version '{current_data}'. Exit the program."
             sys.exit(error_message)
-    # No data specified in argument
+        return True, input_ver, toml
     else:
-        # Increment version in pyproject.toml
-        if patch < 999:
-            patch += 1
-        elif minor < 999:
-            minor += 1
-            patch = 0
-        else:
-            major += 1
-            minor = 0
-            patch = 0
-        new_ver = f"{major}.{minor}.{patch}"
-        create_tag_flag = True
-    return create_tag_flag, new_ver, toml
+        # No update needed if no version is specified
+        return False, current_data, toml
 
 
 if __name__ == "__main__":  # pragma: no cover
     # Check for correct data format
     input_ver = get_arg()
-    # Without argument: Increment version of pyproject.toml
     # With argument: update if version is greater than version of pyproject.toml
+    # Without argument: No update
     create_tag_flag, new_ver, toml = create_ver(input_ver)
     if create_tag_flag:
         update_poetry_project_version(new_ver, toml)
         subprocess.run(["git", "add", "pyproject.toml"])
+    else:
+        print("No version update needed.")
