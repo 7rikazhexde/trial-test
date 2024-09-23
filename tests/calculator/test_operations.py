2dbcf33f2407ea8caab9b61f8c4c536d688cad1d
7rikazhexde <7rikaz.h785.stat2ltas41lcijad@gmail.com>
2024-09-23 20:37:49 +0900

    🔖♻Fix test code
    Update version to 1.0.13

--- tests/calculator/test_operations.py
+++ tests/calculator/test_operations.py
@@ -14,6 +14,7 @@ def test_subtract() -> None:
     assert subtract(5, 3) == 2
     assert subtract(0, 0) == 0
     assert subtract(-2, 2) == -4
+    assert subtract(-1, 2) == -3
 
 
 def test_multiply() -> None:
@@ -21,9 +22,11 @@ def test_multiply() -> None:
     assert multiply(2, 3) == 6
     assert multiply(0, 10) == 0
     assert multiply(-2, 4) == -8
+    assert multiply(-2, 5) == -10
 
 
 def test_divide() -> None:
     print("Running test_divide()")
     assert divide(6, 3) == 2
     assert divide(10, 2) == 5
+    assert divide(7, 2) == 3.5
